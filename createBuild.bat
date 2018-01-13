3bcf64b629b86a2bd584d4c3ea2ba4af95c8fe16
Aarshad Devani <aadevani@deloitte.com>
2018-01-14 00:05:09 +0530

    making changes to folder

--- createBuild.bat
+++ createBuild.bat
@@ -2,5 +2,7 @@
 @cls
 rmdir /s /q FinalBuild
 mkdir FinalBuild
+mkdir FinalBuild\static_assets
 copy "vr\home.html" "FinalBuild\index.html"
 copy "vr\build\*.*" "FinalBuild"
+copy "static_assets\*.*" "FinalBuild\static_assets"
